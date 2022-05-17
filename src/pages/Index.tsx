c0f374797200524991b68a153a28855ca7c81542
Baptiste GAZE <bpt.gaze@gmail.com>
2022-05-17 15:26:47 +0200

    add home content

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,8 +4,9 @@ function Index() {
   return (
     <>
       <Stack spacing="md">
-        <Title order={1}>Index</Title>
-        <Text>Content...</Text>
+        <Title order={1}>Hello!</Title>
+        <Text>Voilà maintenant plusieurs années que je liste chacun des films vus au cinéma sur SensCritique.</Text>
+        <Text>Leur nouvelle API GraphQL permet d'en retirer facilement les informations qui m'ínteresse : Nombre de films vus, dans combien de salles différentes, etc.</Text>
       </Stack>
     </>
   );
